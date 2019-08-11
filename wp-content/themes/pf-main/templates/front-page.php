170421978b186b459a5892bd53d5afb550a83d88
mattras82 <mattras82@gmail.com>
2019-08-11 11:33:38 -0600

    Adding copyright to splash page

--- wp-content/themes/pf-main/templates/front-page.php
+++ wp-content/themes/pf-main/templates/front-page.php
@@ -1,8 +1,11 @@
 <div class="offline-container bg-body left-align">
-    <div class="margin-left-1">
+    <div class="margin-left-1 margin-top-auto">
         <h1>public function</h1>
         <p class="h1">{</p>
         <h2 class="margin-left-1">website coming soon</h2>
         <p class="h1">}<em class="fa fa-i-cursor fa-lg anim8-blink" aria-label="blinking cursor"></em></p>
+    </div>
+    <div class="margin-top-auto align-self-middle">
+        <p class="text-center copyright">Public Function, LLC &copy; <?= date('Y') ?></p>
     </div>
 </div>
